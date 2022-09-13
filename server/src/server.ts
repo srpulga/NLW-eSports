6f46f26f2a383b7f3a46017d2f049b953c7c9e78
Eduardo Galvão <edu.tads@outlook.com>
2022-09-13 20:29:36 -0300

    Update server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,6 +11,6 @@ app.get('/ads', (request, response) => {
   ]);
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(3333, () => {
+  console.log('Server listening on port 3333');
 });
