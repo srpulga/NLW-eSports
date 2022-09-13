ac3655d409b58a020d12f5ecfdce440b91c8f004
Eduardo Galvão <edu.tads@outlook.com>
2022-09-13 20:23:19 -0300

    Create node project

---
+++ server/build/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+app.get('/ads', (request, response) => {
+    return response.json([
+        { id: 1, name: 'Ad 1' },
+        { id: 1, name: 'Ad 2' },
+        { id: 1, name: 'Ad 3' },
+    ]);
+});
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/ads', (request, response) => {
+  return response.json([
+    { id: 1, name: 'Ad 1' },
+    { id: 1, name: 'Ad 2' },
+    { id: 1, name: 'Ad 3' },
+    { id: 1, name: 'Ad 4' },
+  ]);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
