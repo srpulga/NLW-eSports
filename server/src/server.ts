22caeddb1f1fd7538ce64f14549a1711cd262f6e
Eduardo Galvão <edu.tads@outlook.com>
2022-09-15 19:49:53 -0300

    Update routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,24 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
-  return response.json([
-    { id: 1, name: 'Ad 1' },
-    { id: 1, name: 'Ad 2' },
-    { id: 1, name: 'Ad 3' },
-    { id: 1, name: 'Ad 4' },
-  ]);
+app.get('/games', (request, response) => {
+  return response.json([]);
 });
+
+app.post('/ads', (request, response) => {
+  return response.json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  return response.json([]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.json([]);
+});
+
 
 app.listen(3333, () => {
   console.log('Server listening on port 3333');
 });
+ 
